09831b1702dec9ae325b3d0e4f82d1fd90a4dfec
Angelina <linaemalmros@gmail.com>
2023-02-10 08:52:30 +0100

    starting with js to create vs bot game

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let player;
+let bot;
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let playerMove = document.getElementById("playerMove");
+// playerMove.textContent = // skriva ut vad spelaren väljer
+
+function move() {
+    
+}
