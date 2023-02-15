f8711059aba7d7964710698d7e2597991aed35ed
Angelina <linaemalmros@gmail.com>
2023-02-15 11:32:23 +0100

    wip pvp game

---
+++ scripts/game.js
@@ -0,0 +1,7 @@
+let player1Score = 0;
+let player2Score = 0;
+let moves = 0;
+let movesLeft = document.getElementById("movesLeft");
+let result = document.querySelector(".result");
+
+movesLeft.innerHTML = `MOVES LEFT: ${5}`;
