aa5fb6673cbddd80628325c46537f7e5d5ec6f14
Angelina <linaemalmros@gmail.com>
2023-03-01 10:59:44 +0100

    wip join game

--- scripts/opengames.js
+++ scripts/opengames.js
@@ -1,11 +1,31 @@
+
 rpsApi.openGames()
-    .then(data => {
-        data.forEach(game => {
-            const gameToList = `<li>${game.playerOne.username}: ${game.gameStatus}</li>`;
+    .then(games => {
+        games.forEach(game => {
+            const gameToList = `<a href="game.html" onclick="joinGame(gameId)"><li>${game.playerOne.username}: ${game.gameStatus}</li></a>`;
             document.querySelector('#opengames')
                 .insertAdjacentHTML('beforeend', gameToList.toUpperCase())
         })
     })
     .then(
-        // joinGame()
+        // onClick joinGame(gameId)
     )
+
+
+
+
+    // async function listOpenGames() {
+//     let games = await rpsApi.openGames();
+//     let html = '';
+//     games.forEach(game => {
+//         let htmlSegment = `<a href="game.html">
+//                        <li>${game.playerOne.username}: ${game.gameStatus}</li></a>`;
+
+//         html += htmlSegment;
+//     });
+
+//     let container = document.querySelector('.container3');
+//     container.innerHTML = html;
+// }
+
+// listOpenGames();
